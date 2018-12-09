d3e85273987975e9aa4f089361e8af4996f8bb8e
CirrusXx <CirrusXx@users.noreply.github.com>
2018-12-09 21:31:58 +0100

    Update 5
    -dodanie sql lite database
    -poprawione przyciski
    -wyczyszczenie kodu (nowe metody)

---
+++ app/src/main/java/adam/uni/math/lodz/pl/memory/Game.java
@@ -0,0 +1,26 @@
+package adam.uni.math.lodz.pl.memory;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class Game extends AppCompatActivity { 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        ImageView imageView1 = findViewById(R.id.picture1);
+       // imageView1.setImageBitmap(image1);
+
+        }
+    }
+
+
--- app/src/main/java/adam/uni/math/lodz/pl/memory/MainActivity.java
+++ app/src/main/java/adam/uni/math/lodz/pl/memory/MainActivity.java
@@ -2,28 +2,39 @@ package adam.uni.math.lodz.pl.memory;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
+import android.os.Parcel;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    @Override
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    int counter = 1;
+    ImageView imageView1,imageView2,imageView3,imageView4;
+    DataBaseManager dataBase = new DataBaseManager(this);
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         disableReplaceButtons();
-    }
+        imageView1 = findViewById(R.id.image1);
+        imageView2 = findViewById(R.id.image2);
+        imageView3 = findViewById(R.id.image3);
+        imageView4 = findViewById(R.id.image4);
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    int counter = 1;
+    }
 
     public void takePhotoOnClick(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -34,19 +45,17 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
+            int viewID = getResources().getIdentifier("image"+counter,"id",getPackageName());
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView image = findViewById(R.id.image+counter);
+            ImageView image = findViewById(viewID);
             image.setImageBitmap(imageBitmap);
+
             if(counter == 4)
             {
-                return;
+                enableReplaceAndGameActivityButtons();
             }
             counter++;
-            if(counter == 4)
-            {
-                enableReplaceButtons();
-            }
         }
     }
 
@@ -60,25 +69,50 @@ public class MainActivity extends AppCompatActivity {
 
     public void disableReplaceButtons()
     {
-        Button btn1 = (Button) findViewById(R.id.replacePicture1);
-        Button btn2 = (Button) findViewById(R.id.replacePicture2);
-        Button btn3 = (Button) findViewById(R.id.replacePicture3);
-        Button btn4 = (Button) findViewById(R.id.replacePicture4);
+        Button btn1 = findViewById(R.id.replacePicture1);
         btn1.setEnabled(false);
+        Button btn2 = findViewById(R.id.replacePicture2);
         btn2.setEnabled(false);
+        Button btn3 = findViewById(R.id.replacePicture3);
         btn3.setEnabled(false);
+        Button btn4 = findViewById(R.id.replacePicture4);
         btn4.setEnabled(false);
+        Button btn5 = findViewById(R.id.playButton);
+        btn5.setEnabled(false);
     }
 
-    public void enableReplaceButtons()
+    public void enableReplaceAndGameActivityButtons()
     {
-        Button btn1 = (Button) findViewById(R.id.replacePicture1);
+        Button btn1 = findViewById(R.id.replacePicture1);
         btn1.setEnabled(true);
-        Button btn2 = (Button) findViewById(R.id.replacePicture2);
+        Button btn2 = findViewById(R.id.replacePicture2);
         btn2.setEnabled(true);
-        Button btn3 = (Button) findViewById(R.id.replacePicture3);
+        Button btn3 = findViewById(R.id.replacePicture3);
         btn3.setEnabled(true);
-        Button btn4 = (Button) findViewById(R.id.replacePicture4);
+        Button btn4 = findViewById(R.id.replacePicture4);
         btn4.setEnabled(true);
+        Button btn5 = findViewById(R.id.playButton);
+        btn5.setEnabled(true);
+        Button btn6 = findViewById(R.id.takePhotoButton);
+        btn6.setEnabled(false);
     }
+    public void startGameActivity(View view)
+    {
+        BitmapDrawable drawable1 = (BitmapDrawable)imageView1.getDrawable();
+        Bitmap picture1 = drawable1.getBitmap();
+        BitmapDrawable drawable2 = (BitmapDrawable)imageView2.getDrawable();
+        Bitmap picture2 = drawable2.getBitmap();
+        BitmapDrawable drawable3 = (BitmapDrawable)imageView3.getDrawable();
+        Bitmap picture3 = drawable3.getBitmap();
+        BitmapDrawable drawable4 = (BitmapDrawable)imageView4.getDrawable();
+        Bitmap picture4 = drawable4.getBitmap();
+
+
+        Intent intent = new Intent(this,Game.class);
+    //    intent.putExtra("images1",bundle);
+        startActivity(intent);
+    }
+
+
+
 }
